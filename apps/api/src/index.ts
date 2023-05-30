a06a7a021b37255f9ec98b2391e7cc8be87bd685
mallendeo <contacto@chile.sh>
2023-05-30 22:23:52 +0000

    refactor(api): :fire: remove unused validateEnv on index

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,16 +1,7 @@
-import { validateEnv, envalid } from '@jobs/api-util/env'
 import { createFastify } from '@jobs/api-util/server'
 import { createContext } from './context'
 import { appRouter } from './router'
 
 import * as pack from '../package.json'
-
-validateEnv({
-  REDIS_HOST: envalid.str(),
-  POSTGRES_HOST: envalid.str(),
-  POSTGRES_USER: envalid.str(),
-  POSTGRES_PASSWORD: envalid.str(),
-  POSTGRES_DB: envalid.str(),
-})
 
 createFastify(pack.name, appRouter, createContext)
